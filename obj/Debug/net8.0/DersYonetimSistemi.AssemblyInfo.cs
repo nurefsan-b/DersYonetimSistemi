229a646c61ebc76205e0bc05f118e050bd2808d6
nurefşan <nrfsnbzkrt@gmail.com>
2024-12-13 17:59:22 +0300

    first commit

--- obj/Debug/net8.0/DersYonetimSistemi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DersYonetimSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DersYonetimSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ae0a81377aaf615bce4be683918594c8305408")]
 [assembly: System.Reflection.AssemblyProductAttribute("DersYonetimSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DersYonetimSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
